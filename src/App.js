68ca186c448bd687a7848c394a1fd2322b53c18f
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-04 20:03:46 +0100

    implement setTiles method

--- src/App.js
+++ src/App.js
@@ -30,6 +30,39 @@ hero = new Image();
 
 _tile = [];
 
+  setTiles(){
+    //generate empty tiles
+    for(let c=0; c<this.tileColumnCount; c++){
+      this._tile[c] = [];
+      for(let r=0; r<this.tileRowCount; r++){
+          this._tile[c][r] = {
+            x:c*(this.tileW+3),
+            y:r*(this.tileH+3),
+            state:"e"
+          }
+      }
+    }
+
+    //set start tile position
+    let startcolumn = Math.round(this.tileColumnCount/2);
+    let startrow = Math.round(this.tileRowCount/2);
+    this._tile[startcolumn][startrow].state = "s";
+
+    //generate green sprites tile position
+    this.countGreenSprites = this.tileColumnCount > this.tileRowCount ? this.tileColumnCount : this.tileColumnCount < this.tileRowCount ? this.tileRowCount : this.tileColumnCount === this.tileRowCount ? this.tileColumnCount : 0;
+    let greenSprite = 0;
+    while(greenSprite < this.countGreenSprites){
+      let column = (Math.floor(Math.random()*this.tileColumnCount)+1)-1;
+      let row = (Math.floor(Math.random()*this.tileRowCount)+1)-1;
+      if(column!==startcolumn && row!==startrow){
+        if(this._tile[column][row].state !== "w"){
+          this._tile[column][row].state = "w";
+          greenSprite++;
+        }
+      }
+    }
+  }
+
   draw(){
     this.clear();
     for(let c=0; c<this.tileColumnCount; c++){
